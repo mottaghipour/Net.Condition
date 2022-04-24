36ec26e4d7cbf50ba4c4e2d7e2e5c044965a7d38
AmirAbbas <mottaghipour@hotmail.com>
2022-04-24 17:51:10 +0430

    StringConditionHelpers_IsWhiteSpace

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/StringConditionHelpers/StringConditionHelpers.IsWhiteSpace.cs
@@ -0,0 +1,10 @@
+﻿namespace Nilgon.Condition.Helpers
+{
+    public static partial class StringConditionHelpers
+    {
+        public static bool IsWhiteSpace(this string value)
+        {
+            return value == " ";
+        }
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/StringConditionHelpers/StringConditionHelpers.IsWhiteSpace.Tests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.StringConditionHelpers
+{
+    public sealed class StringConditionHelpers_IsWhiteSpace_Tests
+    {
+        [Fact]
+        public void IsWhiteSpace_ReturnsTrue_WhenStringIsWhiteSpace()
+        {
+            string stringToTest = " ";
+
+            bool result = stringToTest.IsWhiteSpace();
+
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void IsWhiteSpace_ReturnsFalse_WhenStringIsFull()
+        {
+            string stringToTest = "test";
+
+            bool result = stringToTest.IsWhiteSpace();
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsWhiteSpace_ReturnsFalse_WhenStringIsNull()
+        {
+            string stringToTest = null;
+
+            bool result = stringToTest.IsWhiteSpace();
+
+            Assert.False(result);
+        }
+    }
+}
