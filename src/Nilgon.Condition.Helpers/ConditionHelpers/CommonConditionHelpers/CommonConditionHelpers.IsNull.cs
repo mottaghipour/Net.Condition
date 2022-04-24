6b8981398fe4d922b56169a82652cebbe09a3fc7
AmirAbbas <mottaghipour@hotmail.com>
2022-04-24 11:10:12 +0430

    CommonConditionHelpers_IsNull

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/CommonConditionHelpers/CommonConditionHelpers.IsNull.cs
@@ -0,0 +1,10 @@
+﻿namespace Nilgon.Condition.Helpers.ConditionHelpers.CommonConditionHelpers
+{
+    public static partial class CommonConditionHelpers
+    {
+        public static bool IsNull<T>(this T value)
+        {
+            return value is null;
+        }
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/CommonConditionHelpers/CommonConditionHelpers.IsNull.Tests.cs
@@ -0,0 +1,28 @@
+﻿using Nilgon.Condition.Helpers.ConditionHelpers.CommonConditionHelpers;
+using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.CommonConditionHelpers
+{
+    public sealed class CommonConditionHelpers_IsNull_Tests
+    {
+        [Fact]
+        public void NullValue_ReturnTrue()
+        {
+            var value = null as string;
+
+            var result = value.IsNull();
+
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void NotNullValue_ReturnFalse()
+        {
+            var value = "test";
+
+            var result = value.IsNull();
+
+            Assert.False(result);
+        }
+    }
+}
