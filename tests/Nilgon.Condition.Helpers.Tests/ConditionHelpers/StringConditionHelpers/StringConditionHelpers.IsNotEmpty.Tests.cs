df0e8318839ba271b5479ba1a0eaac722dc7e18d
AmirAbbas <mottaghipour@hotmail.com>
2022-04-24 11:38:16 +0430

    StringConditionHelpers_IsNotEmpty

---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/StringConditionHelpers/StringConditionHelpers.IsNotEmpty.Tests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.StringConditionHelpers
+{
+    public sealed class StringConditionHelpers_IsNotEmpty_Tests
+    {
+        [Fact]
+        public void IsNotEmpty_ReturnsFalse_WhenStringIsEmpty()
+        {
+            var value = string.Empty;
+
+            var result = value.IsNotEmpty();
+
+            Assert.False(result);
+        }
+        
+        [Fact]
+        public void IsNotEmpty_ReturnsTrue_WhenStringIsNotEmpty()
+        {
+            var value = "test";
+            
+            var result = value.IsNotEmpty();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsNotEmpty_ReturnsFalse_WhenStringIsNull()
+        {
+            var value = null as string;
+
+            var result = value.IsNotEmpty();
+
+            Assert.True(result);
+        }
+    }
+}
