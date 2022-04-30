990744f1feeed50a8ca8378c7faf3af0be11a09f
AmirAbbas <mottaghipour@hotmail.com>
2022-04-30 07:16:23 +0430

    [FEATURE] StringConditionHelpers IsNotNullOrEmpty

---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/StringConditionHelpers/StringConditionHelpers.IsNotNullOrEmpty.Tests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.StringConditionHelpers
+{
+	public sealed class StringConditionHelpers_IsNotNullOrEmpty_Tests
+	{
+		[Fact]
+		public void IsNotNullOrEmpty_ReturnsTrue_WhenStringIsWhiteSpace()
+        {
+			string stringToTest = " ";
+
+			bool result = stringToTest.IsNotNullOrEmpty();
+
+			Assert.True(result);
+        }
+
+		[Fact]
+		public void IsNotNullOrEmpty_ReturnsTrue_WhenStringIsFull()
+		{
+			string stringToTest = "test";
+
+			bool result = stringToTest.IsNotNullOrEmpty();
+
+			Assert.True(result);
+		}
+
+		[Fact]
+		public void IsNotNullOrEmpty_ReturnsFalse_WhenStringIsNull()
+		{
+			string stringToTest = null;
+
+			bool result = stringToTest.IsNotNullOrEmpty();
+
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void IsNotNullOrEmpty_ReturnsFalse_WhenStringIsEmpty()
+		{
+			string stringToTest = "";
+
+			bool result = stringToTest.IsNotNullOrEmpty();
+
+			Assert.False(result);
+		}
+	}
+}
