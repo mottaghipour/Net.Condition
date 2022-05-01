ac388e67eff780d6284bfa7096700177c0e0a89e
AmirAbbas <mottaghipour@hotmail.com>
2022-05-01 12:03:25 +0430

    [FEATURE] StringConditionHelpers => IsNullOrWhiteSpace

---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/StringConditionHelpers/NullOrWhiteSpace_StringConditionHelpers/StringConditionHelpers.IsNullOrWhiteSpace.Tests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.StringConditionHelpers
+{
+    public sealed class StringConditionHelpers_IsNullOrWhiteSpace_Tests
+    {
+        [Fact]
+        public void IsNullOrWhiteSpace_ReturnsTrue_WhenStringIsNull()
+        {
+            string stringToTest = null;
+
+            bool result = stringToTest.IsNullOrWhiteSpace();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsNullOrWhiteSpace_ReturnsTrue_WhenStringIsWhiteSpace()
+        {
+            string stringToTest = " ";
+
+            bool result = stringToTest.IsNullOrWhiteSpace();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsNullOrWhiteSpace_ReturnsFalse_WhenStringIsFull()
+        {
+            string stringToTest = "test";
+
+            bool result = stringToTest.IsNullOrWhiteSpace();
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsNullOrWhiteSpace_ReturnsFalse_WhenStringIsEmpty()
+        {
+            string stringToTest = "";
+
+            bool result = stringToTest.IsNullOrWhiteSpace();
+
+            Assert.False(result);
+        }
+    }
+}
