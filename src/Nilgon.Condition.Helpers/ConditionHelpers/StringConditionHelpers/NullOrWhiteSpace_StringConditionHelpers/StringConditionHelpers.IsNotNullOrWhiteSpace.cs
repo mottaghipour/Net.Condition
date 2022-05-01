ce3f01c29066059010cd1a7bad6773c41a00f345
AmirAbbas <mottaghipour@hotmail.com>
2022-05-01 13:25:00 +0430

    [FEATURE] StringConditionHelper =>IsNotNullOrWhiteSpace

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/StringConditionHelpers/NullOrWhiteSpace_StringConditionHelpers/StringConditionHelpers.IsNotNullOrWhiteSpace.cs
@@ -0,0 +1,10 @@
+﻿namespace Nilgon.Condition.Helpers
+{
+    public static partial class StringConditionHelpers
+    {
+        public static bool IsNotNullOrWhiteSpace(this string value)
+        {
+            return !value.IsNullOrWhiteSpace();
+        }
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/StringConditionHelpers/NullOrWhiteSpace_StringConditionHelpers/StringConditionHelpers.IsNotNullOrWhiteSpace.Tests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.StringConditionHelpers
+{
+    public sealed class StringConditionHelpers_IsNotNullOrWhiteSpace_Tests
+    {
+        [Fact]
+        public void IsNotNullOrWhiteSpace_ReturnsTrue_WhenStringIsFull()
+        {
+            string stringToTest = "Test";
+
+            bool result = stringToTest.IsNotNullOrWhiteSpace();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsNotNullOrWhiteSpace_ReturnsTrue_WhenStringIsEmpty()
+        {
+            string stringToTest = "";
+
+            bool result = stringToTest.IsNotNullOrWhiteSpace();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsNotNullOrWhiteSpace_ReturnsFalse_WhenStringIsNull()
+        {
+            string stringToTest = null;
+
+            bool result = stringToTest.IsNotNullOrWhiteSpace();
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsNotNullOrWhiteSpace_ReturnsFalse_WhenStringIsWhiteSpace()
+        {
+            string stringToTest = " ";
+
+            bool result = stringToTest.IsNotNullOrWhiteSpace();
+
+            Assert.False(result);
+        }
+    }
+}
