4bd13330a19f950fa50045405f1668badaf989b6
AmirAbbas <mottaghipour@hotmail.com>
2022-05-03 21:13:58 +0430

    [FEATURE] Adding xml docs to "StringConditionHelpers" => EmptyHelpers

--- src/Nilgon.Condition.Helpers/ConditionHelpers/StringConditionHelpers/StringConditionHelpers.EmptyHelpers.cs
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/StringConditionHelpers/StringConditionHelpers.EmptyHelpers.cs
@@ -2,6 +2,13 @@
 {
     public static partial class StringConditionHelpers
     {
+        /// <summary>
+        ///    Checks if the <paramref name="value" /> is empty.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>
+        ///    <see langword="true" /> if the <paramref name="value"/> is empty; otherwise, <see langword="false" />.
+        /// </returns>
         public static bool IsEmpty(this string value)
         {
             bool conditionResult = value == string.Empty;
@@ -9,6 +16,13 @@
             return conditionResult;
         }
 
+        /// <summary>
+        ///    Checks if the <paramref name="value" /> is not empty.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>
+        ///    <see langword="true" /> if the <paramref name="value"/> is not empty; otherwise, <see langword="false" />.
+        /// </returns>
         public static bool IsNotEmpty(this string value)
         {
             bool conditionResult = value != string.Empty;
