42be59a692285de7f8be13a6e460127de424d033
AmirAbbas <mottaghipour@hotmail.com>
2022-05-03 21:01:28 +0430

    Revert "[REFACTOR] Cleaning methods "StringConditionHelpers" => NullOrEmptyHelpers"
    This reverts commit 8cc376eea8ddb313ca56f96c21ab3e1057f460d3.

--- src/Nilgon.Condition.Helpers/ConditionHelpers/StringConditionHelpers/StringConditionHelpers.NullOrEmptyHelpers.cs
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/StringConditionHelpers/StringConditionHelpers.NullOrEmptyHelpers.cs
@@ -4,24 +4,10 @@
 	{
 		public static bool IsNullOrEmpty(this string value)
         {
-			bool conditionResult = CheckIsNullOrEmptyCondition(value);
-
-			return conditionResult;
-		}
-
-        private static bool CheckIsNullOrEmptyCondition(string value)
-        {
 			return string.IsNullOrEmpty(value);
         }
 
 		public static bool IsNotNullOrEmpty(this string value)
-		{
-			bool conditionResult = CheckIsNotNullOrEmptyCondition(value);
-
-			return conditionResult;
-		}
-
-		private static bool CheckIsNotNullOrEmptyCondition(string value)
 		{
 			return !string.IsNullOrEmpty(value);
 		}
