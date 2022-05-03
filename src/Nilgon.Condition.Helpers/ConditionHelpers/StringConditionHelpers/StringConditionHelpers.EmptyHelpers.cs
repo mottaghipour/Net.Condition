75600aa3b4e827fbc41514e93901fb16bedb318e
AmirAbbas <mottaghipour@hotmail.com>
2022-05-03 20:52:50 +0430

    [REFACTOR] Cleaning methods "StringConditionHelper" => EmptyHelpers

--- src/Nilgon.Condition.Helpers/ConditionHelpers/StringConditionHelpers/StringConditionHelpers.EmptyHelpers.cs
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/StringConditionHelpers/StringConditionHelpers.EmptyHelpers.cs
@@ -4,10 +4,24 @@
     {
         public static bool IsEmpty(this string value)
         {
+            bool conditionResult = CheckIsEmptyCondition(value);
+
+            return conditionResult;
+        }
+
+        public static bool CheckIsEmptyCondition(string value)
+        {
             return value == string.Empty;
         }
 
         public static bool IsNotEmpty(this string value)
+        {
+            bool conditionResult = CheckIsNotEmptyCondition(value);
+
+            return conditionResult;
+        }
+
+        public static bool CheckIsNotEmptyCondition(string value)
         {
             return value != string.Empty;
         }
