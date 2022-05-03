68e5a6ecee0a49889523776b2899f11d05c16399
AmirAbbas <mottaghipour@hotmail.com>
2022-05-03 20:45:10 +0430

    [FEATURE] Adding xml docs to "CommonConditionHelpers"

--- src/Nilgon.Condition.Helpers/ConditionHelpers/CommonConditionHelpers/CommonConditionHelpers.NullHelpers.cs
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/CommonConditionHelpers/CommonConditionHelpers.NullHelpers.cs
@@ -2,12 +2,42 @@
 {
     public static partial class CommonConditionHelpers
     {
+        /// <summary>
+        ///    Checks if the <paramref name="value" /> is <see langword="null" />.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns>
+        ///    <see langword="true" /> if the <paramref name="value"/> is <see langword="null" />; otherwise, <see langword="false" />.
+        /// </returns>
         public static bool IsNull<T>(this T value)
+        {
+            bool conditionResult = CheckIsNullCondition(value);
+            
+            return conditionResult;
+        }
+
+        private static bool CheckIsNullCondition<T>(T value)
         {
             return value is null;
         }
-        
+
+        /// <summary>
+        ///    Checks if the <paramref name="value" /> is not <see langword="null" />.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns>
+        ///    <see langword="true" /> if the <paramref name="value"/> is not <see langword="null" />; otherwise, <see langword="false" />.
+        /// </returns>
         public static bool IsNotNull<T>(this T value)
+        {
+            bool conditionResult = CheckIsNotNullCondition(value);
+            
+            return conditionResult;
+        }
+
+        private static bool CheckIsNotNullCondition<T>(T value)
         {
             return value is not null;
         }
