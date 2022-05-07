cec238fdd4a191e19c23496a3a2ac579c52d8074
yasin.Z420 <yasin.allahverdi.1386@hotmail.com>
2022-05-07 19:36:34 +0430

    [FEATURE] Adding "ZeroHelpers"  => IntPtrConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/IntPtrConditionHelpers/IntPtrConditionHelpers.ZeroHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Nilgon.Condition.Helpers
+{
+    public static partial class IntPtrConditionHelpers
+    {
+        public static bool IsZero(this nint value)
+        {
+            bool conditionResult = value == IntPtr.Zero;
+
+            return conditionResult;
+        }
+
+        public static bool IsNotZero(this nint value)
+        {
+            bool conditionResult = value != IntPtr.Zero;
+
+            return conditionResult;
+        }
+    }
+}
