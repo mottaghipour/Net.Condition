e910770e455d001746be5aa2d1ad12d5ebd41ce3
yasin.Z420 <yasin.allahverdi.1386@hotmail.com>
2022-05-07 19:57:35 +0430

    [FEATURE] Adding "ZeroHelpers" => UIntPtrConditionHelpers

---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/UIntPtrConditionHelpers/UIntPtrConditionHelpers.ZeroHelpers/IsZeroConditionHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.UIntPtrConditionHelpers.ZeroHelpers
+{
+    public sealed class IsZero_Condition_Helper_Tests
+    {
+        [Fact]
+        public void IsZero_RetunsTrue_WhenNumberIsZero()
+        {
+            nuint numberToTest = UIntPtr.Zero;
+
+            bool result = numberToTest.IsZero();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsZero_ReturnsFalse_WhenNumberIsNotZero()
+        {
+            nuint numberToTest = 1;
+
+            bool result = numberToTest.IsZero();
+
+            Assert.False(result);
+        }
+    }
+}
