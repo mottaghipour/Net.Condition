372a43ae2ec3da5414ff3962a4d7e5fc426bd98b
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 07:42:33 +0430

    [FEATURE] Adding "ZeroHelpers" => ByteConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/ByteConditionHelpers/ByteConditionHelpers.ZeroHelpers.cs
@@ -0,0 +1,19 @@
+﻿namespace Nilgon.Condition.Helpers
+{
+    public static partial class ByteConditionHelpers
+    {
+        public static bool IsZero(this byte value)
+        {
+            bool conditionResult = value == 0;
+
+            return conditionResult;
+        }
+
+        public static bool IsNotZero(this byte value)
+        {
+            bool conditionResult = value != 0;
+
+            return conditionResult;
+        }
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/ByteConditionHelpers/ByteConditionHelpers.ZeroHelpers/IsNotZeroConditionHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.ByteConditionHelpers.ZeroHelpers
+{
+    public sealed class IsNotZero_Condition_Helper_Tests
+    {
+        [Fact]
+        public void IsNotZero_ReturnsTrue_WhenNumberIsNotZero()
+        {
+            byte numberToTest = 1;
+
+            bool result = numberToTest.IsNotZero();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsNotZero_ReturnsFalse_WhenNumberIsZero()
+        {
+            byte numberToTest = 0;
+
+            bool result = numberToTest.IsNotZero();
+
+            Assert.False(result);
+        }
+    }
+}
