9bd7573d7a40908075f0ebc367706727604146ab
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 07:49:25 +0430

    [FEATURE] Adding "ZeroHelpers" => Int16ConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/Int16ConditionHelpers/Int16ConditionHelpers.ZeroHelpers.cs
@@ -0,0 +1,19 @@
+﻿namespace Nilgon.Condition.Helpers
+{
+    public static partial class Int16ConditionHelpers
+    {
+        public static bool IsZero(this short value)
+        {
+            bool conditionResult = value == 0;
+
+            return conditionResult;
+        }
+
+        public static bool IsNotZero(this short value)
+        {
+            bool conditionResult = value != 0;
+
+            return conditionResult;
+        }
+    }
+}
