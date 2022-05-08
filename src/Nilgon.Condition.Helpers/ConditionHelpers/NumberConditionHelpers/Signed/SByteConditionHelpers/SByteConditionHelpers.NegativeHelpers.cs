faca04a02f02cc449fa7e64896e879caee4602d8
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 09:56:19 +0430

    [FEATURE] Adding "NegativeHelpers" => SByteConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/NumberConditionHelpers/Signed/SByteConditionHelpers/SByteConditionHelpers.NegativeHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Nilgon.Condition.Helpers;
+
+public static partial class SByteConditionHelpers
+{
+    public static bool IsNegative(this sbyte value)
+    {
+        bool conditionResult = value < 0;
+
+        return conditionResult;
+    }
+
+    public static bool IsNotNegative(this sbyte value)
+    {
+        bool conditionResult = value > 0;
+
+        return conditionResult;
+    }    
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/NumberConditionHelpers/Signed/SByteConditionHelpers/SByteConditionHelpers.NegativeHelpers/IsNotNegativeConditionHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.SByteConditionHelpers.NegativeHelpers;
+
+public sealed class IsNotNegative_Condition_Helper_Tests
+{
+    [Fact]
+    public void IsNotNegative_ReturnsTrue_WhenNumberIsPositive()
+    {
+        sbyte numberToTest = 1;
+
+        bool result = numberToTest.IsNotNegative();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsNotNegative_ReturnsFalse_WhenNumberIsNegative()
+    {
+        sbyte numberToTest = -1;
+
+        bool result = numberToTest.IsNotNegative();
+
+        Assert.False(result);
+    }
+}
