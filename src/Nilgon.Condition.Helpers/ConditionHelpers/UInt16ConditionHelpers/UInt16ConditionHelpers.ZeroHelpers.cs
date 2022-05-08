c498b3367e1a2d8b6150c688d1d519e1923f226f
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 07:58:52 +0430

    [FEATURE] Adding ""ZeroHelpers" => UInt16ConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/UInt16ConditionHelpers/UInt16ConditionHelpers.ZeroHelpers.cs
@@ -0,0 +1,19 @@
+﻿namespace Nilgon.Condition.Helpers
+{
+    public static partial class UInt16ConditionHelpers
+    {
+        public static bool IsZero(this ushort value)
+        {
+            bool conditionResult = value == 0;
+
+            return conditionResult;
+        }
+
+        public static bool IsNotZero(this ushort value)
+        {
+            bool conditionResult = value != 0;
+
+            return conditionResult;
+        }
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/UInt16ConditionHelpers/UInt16ConditionHelpers.ZeroHelpers/IsNotZeroConditionHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.UInt16ConditionHelpers.ZeroHelpers
+{
+    public sealed class IsNotZero_Condition_Helper_Tests
+    {
+        [Fact]
+        public void IsNotZero_ReturnsTrue_WhenNumberIsNotZero()
+        {
+            ushort numberToTest = 1;
+
+            bool result = numberToTest.IsNotZero();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsZero_ReturnsFalse_WhenNumberIsZero()
+        {
+            ushort numberToTest = 0;
+
+            bool result = numberToTest.IsNotZero();
+
+            Assert.False(result);
+        }
+    }
+}
