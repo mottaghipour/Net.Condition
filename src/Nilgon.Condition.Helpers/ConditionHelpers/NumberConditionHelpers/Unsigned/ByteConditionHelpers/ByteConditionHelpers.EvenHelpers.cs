50eae3485377d2a3731c495833a521617c403c21
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 17:10:56 +0430

    [FEATURE] Adding "EvenHelpers" => ByteConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/NumberConditionHelpers/Unsigned/ByteConditionHelpers/ByteConditionHelpers.EvenHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Nilgon.Condition.Helpers;
+
+public static partial class ByteConditionHelpers
+{
+    public static bool IsEven(this byte value)
+    {
+        bool conditionResult = value % 2 == 0;
+
+        return conditionResult;
+    }
+
+    public static bool IsNotEven(this byte value)
+    {
+        bool conditionResult = value % 2 != 0;
+
+        return conditionResult;
+    }
+}
