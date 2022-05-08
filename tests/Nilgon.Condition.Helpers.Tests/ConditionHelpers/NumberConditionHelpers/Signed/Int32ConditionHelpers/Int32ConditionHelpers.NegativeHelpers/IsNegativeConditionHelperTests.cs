4afd23bfcdfe1d5c473ed735ebd0e6af537e6dee
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 10:05:19 +0430

    [FEATURE] Adding "NegativeHelpers" => Int32ConditionHelpers

---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/NumberConditionHelpers/Signed/Int32ConditionHelpers/Int32ConditionHelpers.NegativeHelpers/IsNegativeConditionHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.Int32ConditionHelpers.NegativeHelpers;
+
+public sealed class IsNegative_Condition_Helper_Tests
+{
+    [Fact]
+    public void IsNegative_ReturnsTrue_WhenNumberIsNegative()
+    {
+        int numberToTest = -1;
+
+        bool result = numberToTest.IsNegative();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsNegative_ReturnsFalse_WhenNumberIsPositive()
+    {
+        int numberToTest = 1;
+
+        bool result = numberToTest.IsNegative();
+
+        Assert.False(result);
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/NumberConditionHelpers/Signed/Int32ConditionHelpers/Int32ConditionHelpers.NegativeHelpers/IsNotNegativeConditionHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.Int32ConditionHelpers.NegativeHelpers;
+
+public sealed class IsNotNegative_Condition_Helper_Tests
+{
+    [Fact]
+    public void IsNotNegative_ReturnsTrue_WhenNumberIsPositive()
+    {
+        int numberToTest = 1;
+
+        bool result = numberToTest.IsNotNegative();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsNotNegative_ReturnsFalse_WhenNumberIsNegative()
+    {
+        int numberToTest = -1;
+
+        bool result = numberToTest.IsNotNegative();
+
+        Assert.False(result);
+    }
+}
