43e5577b6222946e7feade04876e615b33d3c906
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 17:11:35 +0430

    [FEATURE] Adding "EvenHelpers" => UInt64ConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/NumberConditionHelpers/Unsigned/UInt64ConditionHelpers/UInt64ConditionHelpers.EvenHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Nilgon.Condition.Helpers;
+
+public static partial class UInt64ConditionHelpers
+{
+    public static bool IsEven(this ulong value)
+    {
+        bool conditionResult = value % 2 == 0;
+
+        return conditionResult;
+    }
+
+    public static bool IsNotEven(this ulong value)
+    {
+        bool conditionResult = value % 2 != 0;
+
+        return conditionResult;
+    }
+}
