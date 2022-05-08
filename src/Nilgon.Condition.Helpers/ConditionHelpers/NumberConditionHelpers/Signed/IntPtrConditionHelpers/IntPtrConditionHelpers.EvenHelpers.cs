3ef691708ff8b1c006c08b9a51e098384b7f74f2
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 17:10:24 +0430

    [FEATURE] Adding "EvenHelpers" => IntPtrConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/NumberConditionHelpers/Signed/IntPtrConditionHelpers/IntPtrConditionHelpers.EvenHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Nilgon.Condition.Helpers;
+
+public static partial class IntPtrConditionHelpers
+{
+    public static bool IsEven(this nint value)
+    {
+        bool conditionResult = value % 2 == 0;
+
+        return conditionResult;
+    }
+
+    public static bool IsNotEven(this nint value)
+    {
+        bool conditionResult = value % 2 != 0;
+
+        return conditionResult;
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/NumberConditionHelpers/Signed/IntPtrConditionHelpers/IntPtrConditionHelpers.EvenHelpers/IsNotEvenConditionHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.IntPtrConditionHelpers.EvenHelpers;
+
+public sealed class IsNotEven_Condition_Helper_Tests
+{
+    [Fact]
+    public void IsNotEven_ReturnsTrue_WhenNumberIsOdd()
+    {
+        nint numberToTest = 1;
+
+        bool result = numberToTest.IsNotEven();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsNotEven_ReturnsFalse_WhenNumberIsEven()
+    {
+        nint numberToTest = 2;
+
+        bool result = numberToTest.IsNotEven();
+
+        Assert.False(result);
+    }
+}
