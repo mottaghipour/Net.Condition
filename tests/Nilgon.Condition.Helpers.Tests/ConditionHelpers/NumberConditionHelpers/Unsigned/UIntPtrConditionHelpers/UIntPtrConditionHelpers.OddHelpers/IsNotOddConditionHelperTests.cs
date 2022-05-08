a514eccb4f2fefca8921d816390a50c1a68b5181
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 12:53:15 +0430

    [FEATURE] Adding "OddHelpers" => UIntPtrConditionHelpers

---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/NumberConditionHelpers/Unsigned/UIntPtrConditionHelpers/UIntPtrConditionHelpers.OddHelpers/IsNotOddConditionHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.UIntPtrConditionHelpers.OddHelpers;
+
+public sealed class IsNotOdd_Condition_Helper_Tests
+{
+    [Fact]
+    public void IsNotOdd_ReturnsTrue_WhenNumberIsEven()
+    {
+        nuint numberToTest = 2;
+
+        bool result = numberToTest.IsNotOdd();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsNotOdd_ReturnsFalse_WhenNumberIsOdd()
+    {
+        nuint numberToTest = 1;
+
+        bool result = numberToTest.IsNotOdd();
+
+        Assert.False(result);
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/NumberConditionHelpers/Unsigned/UIntPtrConditionHelpers/UIntPtrConditionHelpers.OddHelpers/IsOddConditionHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.UIntPtrConditionHelpers.OddHelpers;
+
+public sealed class IsOdd_Condition_Helper_Tests
+{
+    [Fact]
+    public void IsOdd_ReturnsTrue_WhenNumberIsOdd()
+    {
+        nuint numberToTest = 1;
+
+        bool result = numberToTest.IsOdd();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsOdd_ReturnsFalse_WhenNumberIsEven()
+    {
+        nuint numberToTest = 2;
+
+        bool result = numberToTest.IsOdd();
+
+        Assert.False(result);
+    }
+}
