c108001fbc6cb0d7501ae74c933a6d2efee9537a
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 11:31:53 +0430

    [FEATURE] Adding "PositiveHelpers" => IntPtrConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/NumberConditionHelpers/Signed/IntPtrConditionHelpers/IntPtrConditionHelpers.PositiveHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Nilgon.Condition.Helpers;
+
+public static partial class IntPtrConditionHelpers
+{
+    public static bool IsPositive(this nint value)
+    {
+        bool conditionResult = value > 0;
+
+        return conditionResult;
+    }
+
+    public static bool IsNotPositive(this nint value)
+    {
+        bool conditionResult = value < 0;
+
+        return conditionResult;
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/NumberConditionHelpers/Signed/IntPtrConditionHelpers/IntPtrConditionHelpers.PositiveHelpers/IsNotPositiveConditionHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.IntPtrConditionHelpers.PositiveHelpers;
+
+public sealed class IsNotPositive_Condition_Helper_Tests
+{
+    [Fact]
+    public void IsNotPositive_ReturnsTrue_WhenNumberIsNegative()
+    {
+        nint numberToTest = -1;
+
+        bool result = numberToTest.IsNotPositive();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsNotPositive_ReturnsFalse_WhenNumberIsPostive()
+    {
+        nint numberToTest = 1;
+
+        bool result = numberToTest.IsNotPositive();
+
+        Assert.False(result);
+    }
+}
