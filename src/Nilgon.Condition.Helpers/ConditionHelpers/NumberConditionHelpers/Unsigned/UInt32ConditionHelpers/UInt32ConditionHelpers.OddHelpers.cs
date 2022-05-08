95dfcb9b3fa64353633928b1975afe435f29418d
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 12:52:45 +0430

    [FEATURE] Adding "OddHelpers" => UInt32ConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/NumberConditionHelpers/Unsigned/UInt32ConditionHelpers/UInt32ConditionHelpers.OddHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Nilgon.Condition.Helpers;
+
+public static partial class UInt32ConditionHelpers
+{
+    public static bool IsOdd(this uint value)
+    {
+        bool conditionResult = value % 2 != 0;
+
+        return conditionResult;
+    }
+
+    public static bool IsNotOdd(this uint value)
+    {
+        bool conditionResult = value % 2 == 0;
+
+        return conditionResult;
+    }
+}
