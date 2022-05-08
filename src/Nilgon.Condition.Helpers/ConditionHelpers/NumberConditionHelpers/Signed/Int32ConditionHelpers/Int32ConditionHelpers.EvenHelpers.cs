44adbf88be6eea1a3f064ed289cf0a840e27200f
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 17:09:51 +0430

    [FEATURE] Adding "EvenHelpers" => Int32ConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/NumberConditionHelpers/Signed/Int32ConditionHelpers/Int32ConditionHelpers.EvenHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Nilgon.Condition.Helpers;
+
+public static partial class Int32ConditionHelpers
+{
+    public static bool IsEven(this int value)
+    {
+        bool conditionResult = value % 2 == 0;
+        
+        return conditionResult;
+    }
+
+    public static bool IsNotEven(this int value)
+    {
+        bool conditionResult = value % 2 != 0;
+
+        return conditionResult;
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/NumberConditionHelpers/Signed/Int32ConditionHelpers/Int32ConditionHelpers.EvenHelpers/IsEvenConditionHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.Int32ConditionHelpers.EvenHelpers;
+
+public sealed class IsEven_Condition_Helper_Tests
+{
+    [Fact]
+    public void IsEven_ReturnsTrue_WhenNumberIsEven()
+    {
+        int numberToTest = 2;
+
+        bool result = numberToTest.IsEven();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsEven_ReturnsFalse_WhenNumberIsOdd()
+    {
+        int numberToTest = 1;
+
+        bool result = numberToTest.IsEven();
+
+        Assert.False(result);
+    }
+}
