c679ce9cc98c2ab8384678ac896c5c254129c0fa
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 11:31:09 +0430

    [FEATURE] Adding "PositiveHelpers" => Int16ConditionHelpers

---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/NumberConditionHelpers/Signed/Int16ConditionHelpers/Int16ConditionHelpers.PositiveHelpers/IsNotPositiveConditionHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.Int16ConditionHelpers.PositiveHelpers;
+
+public sealed class IsNotPositive_Condition_Helper_Tests
+{
+    [Fact]
+    public void IsNotPositive_ReturnsTrue_WhenNumberIsNegative()
+    {
+        short numberToTest = -1;
+
+        bool result = numberToTest.IsNotPositive();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsNotPositive_ReturnsFalse_WhenNumberIsPostive()
+    {
+        short numberToTest = 1;
+
+        bool result = numberToTest.IsNotPositive();
+
+        Assert.False(result);
+    }
+}
