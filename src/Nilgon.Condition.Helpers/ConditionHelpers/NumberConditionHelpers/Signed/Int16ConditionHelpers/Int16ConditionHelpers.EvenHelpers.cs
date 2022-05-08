78298e611e0a83ca4912dbcf45b2bc4503028b04
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 17:09:33 +0430

    [FEATURE] Adding "EvenHelpers" => Int16ConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/NumberConditionHelpers/Signed/Int16ConditionHelpers/Int16ConditionHelpers.EvenHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Nilgon.Condition.Helpers;
+
+public static partial class Int16ConditionHelpers
+{
+    public static bool IsEven(this short value)
+    {
+        bool conditionResult = value % 2 == 0;
+
+        return conditionResult;
+    }
+
+    public static bool IsNotEven(this short value)
+    {
+        bool conditionResult = value % 2 != 0;
+
+        return conditionResult;
+    }
+}
