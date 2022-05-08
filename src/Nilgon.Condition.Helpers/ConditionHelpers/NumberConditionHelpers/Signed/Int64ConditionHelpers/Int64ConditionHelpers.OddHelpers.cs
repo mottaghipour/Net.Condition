ca86d5a40f2ebc374454147ae5a104b64d8e5678
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 12:50:57 +0430

    [FEATURE] Adding "OddHelpers" => Int64ConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/NumberConditionHelpers/Signed/Int64ConditionHelpers/Int64ConditionHelpers.OddHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Nilgon.Condition.Helpers;
+
+public static partial class Int64ConditionHelpers
+{
+    public static bool IsOdd(this long value)
+    {
+        bool conditionResult = value % 2 != 0;
+
+        return conditionResult;
+    }
+
+    public static bool IsNotOdd(this long value)
+    {
+        bool conditionResult = value % 2 == 0;
+
+        return conditionResult;
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/NumberConditionHelpers/Signed/Int64ConditionHelpers/Int64ConditionHelpers.OddHelpers/IsNotOddConditionHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.Int64ConditionHelpers.OddHelpers;
+
+public sealed class IsNotOdd_Condition_Helper_Tests
+{
+    [Fact]
+    public void IsNotOdd_ReturnsTrue_WhenNumberIsEven()
+    {
+        long numberToTest = 2;
+
+        bool result = numberToTest.IsNotOdd();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsNotOdd_ReturnsFalse_WhenNumberIsOdd()
+    {
+        long numberToTest = 1;
+        
+        bool result = numberToTest.IsNotOdd();
+
+        Assert.False(result);
+    }
+}
