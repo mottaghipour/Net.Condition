fe15d63b1cd1d4a4e7197601483d1851d32360de
AmirAbbas <mottaghipour@hotmail.com>
2022-05-08 12:51:36 +0430

    [FEATURE] Adding "OddHelpers" => SByteConditionHelpers

---
+++ src/Nilgon.Condition.Helpers/ConditionHelpers/NumberConditionHelpers/Signed/SByteConditionHelpers/SByteConditionHelpers.OddHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Nilgon.Condition.Helpers;
+
+public static partial class SByteConditionHelpers
+{
+    public static bool IsOdd(this sbyte value)
+    {
+        bool conditionResult = value % 2 != 0;
+
+        return conditionResult;
+    }
+
+    public static bool IsNotOdd(this sbyte value)
+    {
+        bool conditionResult = value % 2 == 0;
+
+        return conditionResult;
+    }
+}
---
+++ tests/Nilgon.Condition.Helpers.Tests/ConditionHelpers/NumberConditionHelpers/Signed/SByteConditionHelpers/SByteConditionHelpers.OddHelpers/IsOddConditionHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace Nilgon.Condition.Helpers.Tests.ConditionHelpers.SByteConditionHelpers.OddHelpers;
+
+public sealed class IsOdd_Condition_Helper_Tests
+{
+    [Fact]
+    public void IsOdd_ReturnsTrue_WhenNumberIsOdd()
+    {
+        sbyte numberToTest = 1;
+
+        bool result = numberToTest.IsOdd();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsOdd_ReturnsFalse_WhenNumberIsEven()
+    {
+        sbyte numberToTest = 2;
+
+        bool result = numberToTest.IsOdd();
+
+        Assert.False(result);
+    }
+}
